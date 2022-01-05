95545390ab2ecc4cb554e2b40d6a5d944d9c92f8
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-01-04 21:16:26 -0300

    Updates

---
+++ precache-manifest.2c4d4fbc007e4b63a210639a7c8e2d35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fff6f82c2c3891a6976e523335a8e9",
+    "url": "/trybe-project-trybewallet/index.html"
+  },
+  {
+    "revision": "fb164f6ab9064a63aa62",
+    "url": "/trybe-project-trybewallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b3d90a2c03c9308724d9",
+    "url": "/trybe-project-trybewallet/static/js/2.895d6a48.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-project-trybewallet/static/js/2.895d6a48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb164f6ab9064a63aa62",
+    "url": "/trybe-project-trybewallet/static/js/main.1ca8dfa6.chunk.js"
+  },
+  {
+    "revision": "dbdfce294d603dfd8fb9",
+    "url": "/trybe-project-trybewallet/static/js/runtime-main.ad2de22f.js"
+  }
+]);
